382cb79a194b7e714b75f421464bc1b12fe9bc16
Kanak-dev <kanak24.fv@gmail.com>
2023-09-29 12:25:48 +0545

    docs modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App text-slate-400">
+    <div className="App text-red-500 font-bold font-sans jksdjf jslkdfjskjf jslfkjsd fjlksdjf djsfksjdflk djlkjs fj">
       Hello
     </div>
   );
