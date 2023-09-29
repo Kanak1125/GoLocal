3ffaa7ef222cd47c4eea8f1a10734fbfefbd7568
AshishSatyal <ashishsatyal4@gmail.com>
2023-09-29 19:01:00 +0545

    navbar

--- frontend/src/Pages/Feed.jsx
+++ frontend/src/Pages/Feed.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "../Components/Navbar";
+import Posts from "../Components/Posts";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <div>
+      <Navbar />
+      <Posts />
+    </div>
+  );
 };
 
 export default Feed;
