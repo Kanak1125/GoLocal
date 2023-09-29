cb186fac23de53c3d07aae7dc14dd6723b16ef27
Kanak-dev <kanak24.fv@gmail.com>
2023-09-29 13:39:34 +0545

    routers adde

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PrivateRoute from './authentication/PrivateRoute';
+import PrivateRoute2 from './authentication/PrivateRoute2';
+import Signup from './authentication/SignUp';
+import Login from './authentication/Login';
+import Feed from './Pages/Feed';
+import ForgotPassword from './authentication/ForgotPassword';
 
 function App() {
   return (
-    <div className="App text-red-500 font-bold font-sans jksdjf jslkdfjskjf jslfkjsd fjlksdjf djsfksjdflk djlkjs fj">
-      Hello
-    </div>
+    <BrowserRouter >
+      <Routes>
+        {/* <Route element={<PrivateRoute 
+          componentToRender={<Profile />}
+        />}>
+          <Route path='/user' element={<Profile />} />
+        </Route> */}
+        <Route element={<PrivateRoute2 componentToRender={<Signup />}/>}>
+          <Route path='/signup' element={<Signup />}/>
+        </Route>
+        <Route element={<PrivateRoute2 componentToRender={<Login />}/>}>
+          <Route path='/login' element={<Login />}/>
+        </Route>
+        <Route element={<PrivateRoute2 componentToRender={<ForgotPassword />}/>}>
+          <Route path='/forgot-password' element={<ForgotPassword />}/>
+        </Route>
+        {/* <Route element={<PrivateRoute componentToRender={<UpdateProfile />}/>}>
+          <Route path='/update-profile' element={<UpdateProfile />} />
+        </Route> */}
+        <Route element={<PrivateRoute componentToRender={<Feed />}/>}>
+          <Route path='/' element={<Feed />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
