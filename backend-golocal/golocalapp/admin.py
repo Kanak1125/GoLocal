6acff2029b14ae528230f6e23ddf4fbe69166d4e
Aayush Timalsina <xayush.dc@gmail.com>
2023-09-30 01:23:52 +0545

    Database Schema

--- backend-golocal/golocalapp/admin.py
+++ backend-golocal/golocalapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Post, Comment, Like
+from .models import User, Post, Comment, Like, InteractionCount, Transportation
 
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
@@ -23,3 +23,9 @@ class UserAdmin(BaseUserAdmin):
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(InteractionCount)
+admin.site.register(Transportation)
