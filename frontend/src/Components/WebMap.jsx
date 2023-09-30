a2015021d4327457c1488d29ea850962b5eb9857
Kanak-dev <kanak24.fv@gmail.com>
2023-09-30 15:43:53 +0545

    current user location added to the map

--- frontend/src/Components/WebMap.jsx
+++ frontend/src/Components/WebMap.jsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents, useLocationFound } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import L from "leaflet";
 import { useState } from 'react';
@@ -11,21 +11,46 @@ L.Icon.Default.mergeOptions({
     shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
+// const myMap = L.map('map', {zoomControl: false}).locate({setView: true, maxZoom: 16});
+
+// var marker;
+
+// function onLocationFound(e) { // define a function to handle the locationfound event
+//   if (!marker) { // if the marker is not created yet
+//     marker = L.marker(e.latlng); // create a marker at the current location
+//     marker.addTo(myMap); // add the marker to the map
+//   } else { // if the marker already exists
+//     marker.setLatLng(e.latlng); // update the marker position
+//   }
+// }
+
+// myMap.on('locationfound', onLocationFound);
+
 const initPosition = [51.505, -0.15];
 
 function LocationMarker() {
     const [position, setPosition] = useState(null);
-    const map = useMapEvents({
-        click() {
-            map.locate();
-        },
-        locationfound(e) {
-            setPosition(e.latlng)
-            map.flyTo(e.latlng, map.getZoom())
-        },
-    })
+    // const map = useMapEvents({
+    //     click() {
+    //         map.locate();
+    //     },
+    //     locationfound(e) {
+    //         setPosition(e.latlng)
+    //         map.flyTo(e.latlng, map.getZoom())
+    //     },
+    // })
+    const map = useMap(); // use the useMap hook to access the map instance
+    useMapEvents({ // use the useMapEvents hook to handle map events
+      click() {
+        map.locate();
+      },
+      locationfound(e) { // handle the location found event
+        setPosition(e.latlng); // update the marker position state
+        map.flyTo(e.latlng, map.getZoom()); // center the map on the current location
+      }
+    });
 
-    return position === null ? [55.095, -0.09] : (
+    return position === null ? null : (
         <Marker position={position}>
             <Popup>
                 You are here
