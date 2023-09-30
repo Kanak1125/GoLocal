e213e1d0650734bc97b035e7092a8f5d6ec6bf4b
AshishSatyal <ashishsatyal4@gmail.com>
2023-10-01 05:17:20 +0545

    data from api fetched

--- frontend/src/Components/Posts.jsx
+++ frontend/src/Components/Posts.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PostItem from "./PostItem";
+import axios from "axios";
 
 const Post = (props) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const getDataFromApi = async () => {
+      try {
+        const response = await axios({
+          method: "get",
+          url: "http://127.0.0.1:8000/api/post-create-list/",
+        });
+
+        const data = await response.data;
+        setData([...data]);
+        setLoading(false);
+      } catch (err) {
+        console.log(`ERROR:${err}`);
+        setLoading(false);
+      }
+    };
+
+    getDataFromApi();
+    console.log(data);
+  }, []);
+
   const { open, openModal, closeModal } = props;
   return <PostItem open={open} openModal={openModal} closeModal={closeModal} />;
 };
