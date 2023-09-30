a054b30978c78698551abcd42c81d7c7426ba3d7
AshishSatyal <ashishsatyal4@gmail.com>
2023-09-30 08:56:39 +0545

    auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,36 +6,40 @@ import Signup from "./authentication/SignUp";
 import Login from "./authentication/Login";
 import Feed from "./Pages/Feed";
 import ForgotPassword from "./authentication/ForgotPassword";
-import Post from "./Pages/Post";
 import AuthProvider from "./context/AuthContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* <Route element={<PrivateRoute 
-          componentToRender={<Profile />}
-        />}>
-          <Route path='/user' element={<Profile />} />
-        </Route> */}
-        <Route element={<PrivateRoute2 componentToRender={<Signup />} />}>
-          <Route path='/signup' element={<Signup />} />
-        </Route>
-        <Route element={<PrivateRoute2 componentToRender={<Login />} />}>
-          <Route path='/login' element={<Login />} />
-        </Route>
-        <Route
-          element={<PrivateRoute2 componentToRender={<ForgotPassword />} />}
-        >
-          <Route path='/forgot-password' element={<ForgotPassword />} />
-        </Route>
-        {/* <Route element={<PrivateRoute componentToRender={<UpdateProfile />}/>}>
-          <Route path='/update-profile' element={<UpdateProfile />} />
-        </Route> */}
-        <Route element={<PrivateRoute componentToRender={<Feed />} />}>
+      <AuthProvider>
+        <Routes>
+          {/* <Route element={<PrivateRoute 
+            componentToRender={<Profile />}
+          />}>
+            <Route path='/user' element={<Profile />} />
+          </Route> */}
+          <Route element={<PrivateRoute2 componentToRender={<Signup />} />}>
+            <Route path='/signup' element={<Signup />} />
+          </Route>
+          <Route element={<PrivateRoute2 componentToRender={<Login />} />}>
+            <Route path='/login' element={<Login />} />
+          </Route>
+          <Route
+            element={<PrivateRoute2 componentToRender={<ForgotPassword />} />}
+          >
+            <Route path='/forgot-password' element={<ForgotPassword />} />
+          </Route>
+          {/* <Route element={<PrivateRoute componentToRender={<UpdateProfile />}/>}>
+            <Route path='/update-profile' element={<UpdateProfile />} />
+          </Route> */}
+          {/* <Route element={<PrivateRoute componentToRender={<Feed />}/>}> */}
           <Route path='/' element={<Feed />} />
-        </Route>
-      </Routes>
+          {/* </Route> */}
+          <Route element={<PrivateRoute componentToRender={<Post />} />}>
+            <Route path='/post' element={<Post />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
