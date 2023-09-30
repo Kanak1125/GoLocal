a25c266bf7a0ec754365998e7e2f01f298f9aa5c
AshishSatyal <ashishsatyal4@gmail.com>
2023-09-30 15:45:30 +0545

    marker coordinates

--- frontend/src/Components/WebMap.jsx
+++ frontend/src/Components/WebMap.jsx
@@ -1,53 +1,74 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css';
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-import { useState } from 'react';
+import { useState } from "react";
 
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
 const initPosition = [51.505, -0.15];
 
 function LocationMarker() {
-    const [position, setPosition] = useState(null);
-    const map = useMapEvents({
-        click() {
-            map.locate();
-        },
-        locationfound(e) {
-            setPosition(e.latlng)
-            map.flyTo(e.latlng, map.getZoom())
-        },
-    })
+  const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
 
-    return position === null ? [55.095, -0.09] : (
-        <Marker position={position}>
-            <Popup>
-                You are here
-            </Popup>
-        </Marker>
-    )
+  return position === null ? (
+    [55.095, -0.09]
+  ) : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
 }
+
+// return marker location cordinates
+const Markers = () => {
+  const marker = "";
+  const map = useMapEvents({
+    click(e) {
+      const newMarker = e.latlng;
+      marker = newMarker;
+    },
+  });
+
+  return <Marker position={marker}></Marker>;
+};
+
 const WebMap = () => {
   return (
-    <MapContainer 
-      center={initPosition} 
-      zoom={13} 
-      scrollWheelZoom={true} 
-      style={{'height': '500px', 'width': '500px'}}
+    <MapContainer
+      center={initPosition}
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ height: "500px", width: "500px" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
       <LocationMarker />
     </MapContainer>
-  )
-}
+  );
+};
 
-export default WebMap
+export default WebMap;
