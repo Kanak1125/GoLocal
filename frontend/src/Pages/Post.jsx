bc6892e4998c97bece6ec21e6e8b50724981ece3
Kanak-dev <kanak24.fv@gmail.com>
2023-09-30 11:42:29 +0545

    styled post form

--- frontend/src/Pages/Post.jsx
+++ frontend/src/Pages/Post.jsx
@@ -27,10 +27,10 @@ const Post = () => {
     formData.description
   );
   return (
-    <div className=' min-h-screen flex items-center justify-center w-full'>
+    <div className='min-w-[280px] max-w-[444px] min-h-screen flex items-center justify-center w-full my-4 mx-auto'>
       <div className='container'>
         <div className='location-accessbility'>
-          <form>
+          <form className="py-4 px-2">
             <div className='loaction-name'>
               <label htmlFor='location'>
                 Location
@@ -43,7 +43,7 @@ const Post = () => {
                 />
               </label>
               <div className='map'>
-                <button className='accent-color text-white font-bold py-2 px-4 rounded-full transition-all duration-300'>
+                <button className='accent-color text-white font-bold py-2 px-4 rounded-full transition-all duration-300 my-3'>
                   Add location From Map
                 </button>
               </div>
@@ -53,6 +53,7 @@ const Post = () => {
               <select
                 name='transport'
                 id='transport'
+                className="mb-4"
                 value={formData.transport}
                 onChange={handleChange}
               >
@@ -65,6 +66,7 @@ const Post = () => {
               <select
                 name='restaurant'
                 id='restaurant'
+                className="mb-4"
                 value={formData.restaurant}
                 onChange={handleChange}
               >
@@ -76,6 +78,7 @@ const Post = () => {
               <select
                 name='lodging'
                 id='lodging'
+                className="mb-4"
                 value={formData.lodging}
                 onChange={handleChange}
               >
@@ -84,7 +87,7 @@ const Post = () => {
                 <option value='Not-Available'>Not Available</option>
               </select>
             </div>
-            <div className='discription-field'>
+            <div className='discription-field w-full'>
               <textarea
                 className='w-full px-3 py-2 border rounded-lg focus:ring focus:ring-blue-300 focus:border-blue-500'
                 onChange={handleChange}
@@ -93,10 +96,10 @@ const Post = () => {
                 name='description'
               />
             </div>
-            <div className='trek-toggle min-h-screen flex items-center w-full'>
+            <div className='trek-toggle flex items-center w-full'>
               <ToggleSlider />
             </div>
-            <input type='submit' value='Upload' />
+            <input type='submit' value='Upload' className="accent-color text-white font-bold py-2 px-4 rounded-full transition-all duration-300 my-3 cursor-pointer"/>
           </form>
         </div>
       </div>
