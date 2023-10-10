9e8bcf0540862650ffcd446a3ce60e8a8f66fb85
AshishSatyal <ashishsatyal4@gmail.com>
2023-10-10 16:23:23 +0545

    navbar changes

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Logo from "../assets/logo.png";
+import PP from "../assets/pp.png";
+
 import { FaHome } from "react-icons/fa";
 import Search from "./Search";
 import { useAuthContext } from "../context/AuthContext";
@@ -24,16 +26,24 @@ const Navbar = () => {
   return (
     <div className='container max-w-[1400px] py-2 mx-auto bg-white'>
       <nav className='nav-container flex justify-around '>
-        <Link to='/' className='logo w-20  m-2 text-white hidden md:block'>
+        <div className="nav-left flex items-center">
+           <Link to='/' className='logo w-20  m-2 text-white hidden md:block'>
           <img src={Logo} alt='logo-img' />
-        </Link>
+           </Link>
+          <Search searchHandler={handleChange} searchInput={searchInput} />
+
+        </div>
         <div className='nav-links flex items-center'>
           <Link to={'/'} className="hidden md:block">
             <FaHome size={24} />
           </Link>
-          <Search searchHandler={handleChange} searchInput={searchInput} />
         </div>
         <div className='nav-right flex items-center'>
+          <div className="profile circle w-[40px] min-w-[40px] h-[40px] rounded-full accent-color overflow-hidden mr-5">
+
+            <img src={PP} alt="profile-img" className="w-full h-full" />
+            
+          </div>
           <Link 
             className='accent-color text-white font-bold py-2 px-4 rounded-full transition-all duration-300'
             to={`${currentUser ? '/login' : 'signup'}`} 
